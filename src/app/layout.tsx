0b2f3c174a1e3140498bc6c63d1e45f72be0cd6d
Starboy <samskywalker001@gmail.com>
2024-10-31 10:04:39 +0000

    initial ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,18 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Next Gallery",
+  title: "Image Gallery",
   description: "Next Gallery with Next.js + Create T3 App by @skywalkerSam",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 export function TopNav() {
   return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
-      <div >Sign In</div>
+    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
+      {" "}
+      {/* border-b */}
+      <div></div>
+      <div>Sign In</div>
     </nav>
   );
 }
@@ -23,7 +25,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body className="flex flex-col gap-4 bg-black text-gray-300">
+      <body className="flex flex-col gap-4 bg-black text-gray-400">
         <TopNav />
         {children}
       </body>
