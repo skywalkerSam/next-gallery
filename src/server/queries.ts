ffbfe916e0cc15c47d8f29ab6044919f6feb886c
Starboy <samskywalker001@gmail.com>
2025-01-21 07:49:55 +0530

    feature: personal gallery

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -1,9 +1,19 @@
 import "server-only";
 import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
 
 export async function getUserImages() {
-  const images = await db.query.images.findMany({
-    orderBy: (order, { asc }) => asc(order.id), // desc, nice.)
-  });
-  return images;
+  // using auth() for userId validation
+  const user = await auth();
+
+  if (user.userId) {
+    const images = await db.query.images.findMany({
+      // filter
+      where: (model, { eq }) => eq(model.userId, user.userId),
+      orderBy: (model, { asc }) => asc(model.id), // desc, nice.)
+    });
+    return images;
+  } else {
+    throw new Error("User not found!");
+  }
 }
