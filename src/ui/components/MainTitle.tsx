e7aeb94ebffacfab012a362663822b262c257df1
Starboy <samskywalker001@gmail.com>
2025-02-18 01:43:55 +0530

    font fix

--- src/ui/components/MainTitle.tsx
+++ src/ui/components/MainTitle.tsx
@@ -7,7 +7,8 @@ import NextLogo from "./NextLogo";
 export default function MainTitle() {
   return (
     <div>
-      <h1 className="row-start-3 flex flex-wrap items-center justify-center gap-3 text-slate-500 sm:text-[4rem]">
+      <h1 className="row-start-3 flex flex-wrap items-center justify-center gap-3 text-slate-500 text-6xl">
+      {/* <h1 className="row-start-3 flex flex-wrap items-center justify-center gap-3 text-slate-500 sm:text-[4rem]"> */}
         Image <span className="text-blue-400">Gallery</span> w/{" "}
         {/* <span className="text-black tracking-tighter">NEXT.JS</span> */}
         <NextLogo></NextLogo>
