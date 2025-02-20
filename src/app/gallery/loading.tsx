916a427554fce46d1b005de407172ce387fa8493
Starboy <samskywalker001@gmail.com>
2025-02-20 15:10:55 +0530

    suspense overhaul

--- src/app/gallery/loading.tsx
+++ src/app/gallery/loading.tsx
@@ -1,17 +1,13 @@
-const centeredDivStyle =
-  "flex flex-row min-h-screen justify-center items-center";
-const loadingStyle = "h-[475px] w-[475px] animate-pulse bg-slate-900";
-const gridStyle = "grid grid-cols-3 gap-3";
-
+import { galleryStyles } from "~/styles/styles";
 // animation
 const loadingAnimation = [...Array.from({ length: 9 })].map((_, i) => (
-  <div key={i} className={loadingStyle} />
+  <div key={i} className={galleryStyles.loading} />
 ));
 
 export default function GalleryLoading() {
   return (
-    <div className={centeredDivStyle}>
-      <div className={gridStyle}>{loadingAnimation}</div>
+    <div className={galleryStyles.centeredDiv}>
+      <div className={galleryStyles.grid}>{loadingAnimation}</div>
     </div>
   );
 }
