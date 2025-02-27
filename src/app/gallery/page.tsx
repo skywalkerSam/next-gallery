57080e6a29dc4bfd81181db0934714a933afd0d7
Starboy <samskywalker001@gmail.com>
2025-02-27 13:24:53 +0000

    finishing touches

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -9,134 +9,40 @@ import ImageUploadButton from "~/components/ui/gallery/default-upload-button";
 // dynamic behavior
 export const dynamic = "force-dynamic";
 
-// const loadingStyle = [...Array.from({ length: 9 })].map((_, i) => (
-//   <div key={i} className="h-[475px] w-[475px] animate-pulse bg-slate-900" />
-// ));
-
-// // images w/ uploadthing
-// const testUrls: string[] = [
-//   "https://utfs.io/f/suXlWOYj6P5mlyveTv2FdBCMI6LznvwAF7HrjlGR8kmbJY5K",
-//   "https://utfs.io/f/suXlWOYj6P5mgdRzw1a0DF9hfBImAUEWdQ0GqoYgt2Ta1CJr",
-//   "https://utfs.io/f/suXlWOYj6P5mfqCcKs26pjHoZscJI1UuBOgFKiEMSa3XGvqx",
-//   "https://utfs.io/f/suXlWOYj6P5mFJe8r1mkT4re6FHUXmsdiP1v7bMD5jWBGg0A",
-//   "https://utfs.io/f/suXlWOYj6P5mGP0uXcVWCxR45grP3wq2Un6tIcayuhofNY9V",
-//   "https://utfs.io/f/suXlWOYj6P5meWD2yG7xj2cVFsz0dw8MHNDSt19mhyQLKigI",
-//   "https://utfs.io/f/suXlWOYj6P5mCRCWOshy9hp3d8uf7xJznlReXIsNiM5B12DA",
-//   "https://utfs.io/f/suXlWOYj6P5mqtNWj4LMPy9d0NzxhwsMlt8SWb1ojm5pUIEv",
-//   "https://utfs.io/f/suXlWOYj6P5mJc0GtgUPtN9DlEFPjdUmXnCsaZ6u01K7LMih",
-//   "https://utfs.io/f/suXlWOYj6P5m39Pb8zsJQi4BAbP5WzmlFnh81v9OcNYVtRyZ",
-//   "https://utfs.io/f/suXlWOYj6P5m0oRoWEJxwFpHSrVeunNyC7TQMXJsRdzm4bxY",
-//   "https://utfs.io/f/suXlWOYj6P5mh0uTBjHLuq3fIbErM1Xl482VAz6H9QYDJgRK",
-// ];
-
-// // image IDs
-// const testImages: Array<{ id: number; url: string }> = testUrls.map(
-//   (url, index) => ({
-//     id: index++,
-//     url,
-//   }),
-// );
-
 export default async function Page() {
-  // Moved to server
-  // const images = await db.query.images.findMany({
-  //   orderBy: (order, { asc }) => asc(order.id), // desc, nice.)
-  // });
   const images = await getUserImages();
 
   return (
-    <main className="to-grey flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-black text-gray-500">
+    <main className="to-grey flex min-h-screen flex-col items-center justify-center text-slate-600">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        {/* <h1 className="text-5xl font-extrabold tracking-tight text-gray-300 sm:text-[5rem]">
-          Image <span className="text-[hsl(207,100%,70%)]">Gallery</span> w/{" "}
-          <span className="text-[hsl(0,0%,11%)]">NEXT</span>
-          </h1> */}
         <div className="row-start-1 mt-0 flex flex-wrap items-center justify-center gap-2 p-3">
-          {/* <Suspense fallback={<p className="text-slate-600 text-6xl">Fetching Images...</p>}> */}
-          {/* <Suspense
-            fallback={
-              <div className="grid grid-cols-3 gap-4">
-                {loadingStyle}
-              </div>
-            }
-          > */}
-            {/* prod */}
-            {images.map((image) => (
-              <div key={image.id}>
-                <Link
-                  className="card"
-                  key={image.id}
-                  href={`/images/${image.id}`}
-                  passHref
-                >
-                  {/* NOTE: Opt out of image optimization for images < 1KB, SVGs, or GIFs as they don't get the benefits */}
-                  <Image
-                    src={image.url}
-                    alt={image.name}
-                    width={475}
-                    height={475}
-                    style={{ objectFit: "contain" }}
-                    loading="lazy"
-                  ></Image>
-                  {/* {image.name} */}
-                </Link>
-              </div>
-            ))}
-          {/* </Suspense> */}
-          {/* for testing */}
-          {/* shouldn't have used index as a key but, just to keep thy console clear & '-' to counter weird JS behaviors.) */}
-          {/* {images.map((image, i) => (
-            <div key={image.id + "-" + i}>
-              <Image
-                src={image.url}
-                alt="Image"
-                width={450}
-                height={450}
-              ></Image>
-              {image.name}
-            </div>
-          ))} */}
-          {/* multiple pages test */}
-          {/* {[...testImages, ...testImages].map((image) => (
+          {images.map((image) => (
             <div key={image.id}>
-              <Image
-                src={image.url}
-                alt="Image"
-                width={450}
-                height={450}
-              ></Image>
+              <Link
+                className="card"
+                key={image.id}
+                href={`/images/${image.id}`}
+                passHref
+              >
+                <Image
+                  src={image.url}
+                  alt={image.name}
+                  width={475}
+                  height={475}
+                  style={{ objectFit: "contain" }}
+                  loading="lazy"
+                ></Image>
+                {/* {image.name} */}
+              </Link>
             </div>
-          ))} */}
+          ))}
         </div>
 
         {/* uploadthing image uploads */}
         <ImageUploadButton></ImageUploadButton>
-
-        {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div> */}
       </div>
     </main>
   );
 }
+
+// NOTE: Opt out of image optimization for images < 1KB, SVGs, or GIFs as they don't get the benefits.)
--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -7,4 +7,5 @@ export const galleryStyles = {
   centeredDiv: "flex flex-row min-h-screen justify-center items-center",
   loading: "h-[475px] w-[475px] animate-pulse bg-slate-900",
   grid: "grid grid-cols-3 gap-3",
+  gradiantBackground: "bg-gradient-to-b from-blue-950 to-black",
 } as const;
