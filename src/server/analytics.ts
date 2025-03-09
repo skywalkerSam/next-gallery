a02bd2d31ebff082d5bc581770a0bdc198d2d059
Starboy <samskywalker001@gmail.com>
2025-03-09 10:15:50 +0000

    error handling

--- src/server/analytics.ts
+++ src/server/analytics.ts
@@ -2,9 +2,17 @@ import "server-only";
 
 import { PostHog } from "posthog-node";
 
+const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+const posthogHost = process.env.NEXT_PUBLIC_POSTHOG_HOST;
+
 export default function PostHogServerClient() {
-  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  if (!posthogKey) {
+    throw new Error("PostHog key not found!");
+  } else if (!posthogHost) {
+    throw new Error("PostHog host not found!");
+  }
+  const posthogClient = new PostHog(posthogKey, {
+    host: posthogHost,
     flushAt: 1,
     flushInterval: 0,
   });
