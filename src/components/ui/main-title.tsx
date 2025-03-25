10c21dcf3b7514fe5d3b902502597ec8b7c7b90f
Starboy <samskywalker001@gmail.com>
2025-03-25 00:42:11 +0000

    ui optimizations

--- src/components/ui/main-title.tsx
+++ src/components/ui/main-title.tsx
@@ -5,7 +5,7 @@ import NextLogo from "./next-logo";
 // Styles
 const centeredDiv = "flex min-h-screen flex-row items-center justify-center";
 const titleStyle =
-  "flex p-3 gap-3 text-4xl md:text-6xl tracking-tighter md:tracking-normal";
+  "flex p-3 gap-3 text-5xl md:text-6xl tracking-tighter md:tracking-normal";
 const nextLogoStyle = "pt-1 space-y-2 w-36 md:w-60";
 const textStyle =
   "mb-8 bg-gradient-to-r from-blue-400 via-purple-500 to-cyan-400 bg-clip-text font-bold text-transparent";
