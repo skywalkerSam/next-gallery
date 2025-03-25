3a571b0a1c31e7101b2a941e48af8460c57c6ea6
Starboy <samskywalker001@gmail.com>
2025-03-25 00:38:35 +0000

    ui optimizations

--- src/components/ui/next-logo.tsx
+++ src/components/ui/next-logo.tsx
@@ -1,30 +1,13 @@
 "use client";
+
 import type { JSX } from "react";
 import Image from "next/image";
 
-// const centeredDivStyle = "flex items-center justify-center p-3";
-// const spanStyle = "text-slate-950 hover:text-slate-600 dark:invert";
-// const nextStyle = "text-5xl font-semibold";
-// const jsStyle = "text-lg tracking-tighter";
-
 export default function NextLogo(): JSX.Element {
   return (
-    // <div className={centeredDivStyle}>
     <div>
-      {/* <span className={spanStyle}>
-        <span className={nextStyle}>
-          <a
-            href="https://nextjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            NEXT<span className={jsStyle}>.JS</span>
-          </a>
-        </span>
-      </span> */}
       <a href="https://nextjs.org" target="_blank" rel="noopener noreferrer">
         <Image
-          // className="dark:invert m-1"
           className="m-1 transition-opacity hover:opacity-80 dark:invert"
           src="/next.svg"
           alt="Next.js logo"
--- src/components/ui/top-bar.tsx
+++ src/components/ui/top-bar.tsx
@@ -3,37 +3,34 @@
 import Link from "next/link";
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
-// import ImageUploadButton from "../gallery/default-upload-button";
 import "~/styles/starboy-logo.css";
 import type { JSX } from "react";
 import UploadButton from "./upload-button";
 
-const topBarStyle = "flex justify-between p-3";
-
 export default function TopBar(): JSX.Element {
   return (
-    <div>
-      <header className={topBarStyle}>
-        {/* <h1>Starboy Inc.</h1> */}
-        {/* <Link href="/" className="hover:text-blue-400 hover:underline"> */}
-        <Link href="/">
-          {/* Starboy Inc. */}
-          <Image
-            src="/icon.svg"
-            alt="Starboy Logo"
-            width={90}
-            height={60}
-            className="starboy-logo p-3"
-            priority
-          ></Image>
-        </Link>
-        {/* uploadthing image uploads */}
-        {/* <ImageUploadButton></ImageUploadButton> */}
+    <div className="flex">
+      <div className="flex size-9 flex-grow">
+        <div className="w-20">
+          <Link href="/">
+            {/* Starboy Inc. */}
+            <Image
+              src="/icon.svg"
+              alt="Starboy Logo"
+              width={90}
+              height={90}
+              className="starboy-logo p-3"
+              priority
+            ></Image>
+          </Link>
+        </div>
+      </div>
+      <div className="flex size-9 flex-grow">
         <UploadButton></UploadButton>
-        {/* <UserButton showName></UserButton> */}
-        {/* <UserButton appearance={{elements: {avatarImage: {}}}}></UserButton> */}
+      </div>
+      <div className="flex size-9 flex-none p-10">
         <UserButton></UserButton>
-      </header>
+      </div>
     </div>
   );
 }
