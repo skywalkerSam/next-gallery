734c7c7ba3c6bbd34a48c77f7cdab74df57eb090
Starboy <samskywalker001@gmail.com>
2026-01-05 01:50:20 +0000

    usage: git [-v | --version] [-h | --help] [-C <path>] [-c <name>=<value>]            [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]            [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]            [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]            [--config-env=<name>=<envvar>] <command> [<args>]
    These are common Git commands used in various situations:
    
    start a working area (see also: git help tutorial)
       clone     Clone a repository into a new directory
       init      Create an empty Git repository or reinitialize an existing one
    
    work on the current change (see also: git help everyday)
       add       Add file contents to the index
       mv        Move or rename a file, a directory, or a symlink
       restore   Restore working tree files
       rm        Remove files from the working tree and from the index
    
    examine the history and state (see also: git help revisions)
       bisect    Use binary search to find the commit that introduced a bug
       diff      Show changes between commits, commit and working tree, etc
       grep      Print lines matching a pattern
       log       Show commit logs
       show      Show various types of objects
       status    Show the working tree status
    
    grow, mark and tweak your common history
       branch    List, create, or delete branches
       commit    Record changes to the repository
       merge     Join two or more development histories together
       rebase    Reapply commits on top of another base tip
       reset     Reset current HEAD to the specified state
       switch    Switch branches
       tag       Create, list, delete or verify a tag object signed with GPG
    
    collaborate (see also: git help workflows)
       fetch     Download objects and refs from another repository
       pull      Fetch from and integrate with another repository or a local branch
       push      Update remote refs along with associated objects
    
    'git help -a' and 'git help -g' list available subcommands and some
    concept guides. See 'git help <command>' or 'git help <concept>'
    to read about a specific subcommand or concept.
    See 'git help git' for an overview of the system. to

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <>
       <div className={"flex items-center justify-center min-h-screen text-5xl md:text-6xl xl:text-7xl"}>
-        <h1><span className={"hover:text-sky-500"}>i</span>mage <span className={"hover:text-sky-500"}>g</span>allery</h1>
+        <h1><span className={"hover:text-sky-500"}>i</span>mage <span className={"hover:text-sky-500"}>G</span>allery</h1>
       </div>
     </>
   )
