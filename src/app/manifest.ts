408aac80c1a4f1476095d6a789ab3c417cac1beb
Starboy <samskywalker001@gmail.com>
2026-01-10 12:47:27 +0000

    icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,54 +1,54 @@
-import type {MetadataRoute} from "next";
+import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
-    return {
-        name: "image Gallery",
-        short_name: "Gallery",
-        description: "A Personalized Gallery Application Demo",
-        start_url: "/",
-        display: "standalone",
-        // theme_color: "#000000",
-        // theme_color: "#bae6fd",
-        // theme_color: "#1e1e1e",
-        theme_color: "#121212", // Softer black
-        background_color: "#000000",
-        orientation: "portrait",
-        scope: "/",
-        categories: ["photography", "gallery", "art"],
-        shortcuts: [
-            {
-                name: "Gallery Home",
-                short_name: "Home",
-                description: "image Gallery Home",
-                url: "/gallery",
-                icons: [{src: "/web-app-manifest-192x192.png", sizes: "192x192"}],
-            },
-        ],
-        icons: [
-            {
-                src: "/web-app-manifest-192x192.png",
-                sizes: "192x192",
-                type: "image/png",
-                purpose: "maskable",
-            },
-            {
-                src: "/web-app-manifest-512x512.png",
-                sizes: "512x512",
-                type: "image/png",
-                purpose: "maskable",
-            },
-            {
-                src: "/web-app-manifest-192x192.png",
-                sizes: "192x192",
-                type: "image/png",
-                purpose: "any",
-            },
-            {
-                src: "/web-app-manifest-512x512.png",
-                sizes: "512x512",
-                type: "image/png",
-                purpose: "any",
-            },
-        ],
-    };
+  return {
+    name: "image Gallery",
+    short_name: "Gallery",
+    description: "A Personalized Gallery Application Demo",
+    start_url: "/",
+    display: "standalone",
+    // theme_color: "#000000",
+    // theme_color: "#bae6fd",
+    // theme_color: "#1e1e1e",
+    theme_color: "#121212", // Softer black
+    background_color: "#000000",
+    orientation: "portrait",
+    scope: "/",
+    categories: ["photography", "gallery", "images"],
+    shortcuts: [
+      {
+        name: "Gallery Home",
+        short_name: "Home",
+        description: "image Gallery Home",
+        url: "/gallery",
+        icons: [{ src: "/gallery.svg" }],
+      },
+    ],
+    icons: [
+      {
+        src: "/gallery.svg",
+        sizes: "192x192",
+        type: "svg",
+        purpose: "maskable",
+      },
+      {
+        src: "/gallery.svg",
+        sizes: "512x512",
+        type: "svg",
+        purpose: "maskable",
+      },
+      {
+        src: "/gallery.svg",
+        sizes: "192x192",
+        type: "svg",
+        purpose: "any",
+      },
+      {
+        src: "/gallery.svg",
+        sizes: "512x512",
+        type: "svg",
+        purpose: "any",
+      },
+    ],
+  };
 }
